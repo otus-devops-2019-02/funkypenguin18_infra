56b2c08803db85c8282f1c5fe6892a51291e77a4
funkypenuin18 <odanilov@it-atlantic.com>
2019-04-13 15:05:41 +0300

    Terraform 2

--- terraform/stage/variables.tf
+++ terraform/stage/variables.tf
@@ -17,12 +17,12 @@ variable private_key_path {
 
 variable app_disk_image {
   description = "Disk image for reddit app"
-  default = "reddit-app-base"
+  default = "reddit-app"
 }
 
 variable db_disk_image {
   description = "Disk image for reddit db"
-  default = "reddit-db-base"
+  default = "reddit-db"
 }
 
 variable zone {
