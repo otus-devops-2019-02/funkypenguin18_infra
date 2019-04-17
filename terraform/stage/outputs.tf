b22865702eebf81b364d1124f59ae6349b850716
funkypenuin18 <odanilov@it-atlantic.com>
2019-04-17 16:22:21 +0300

    ansible-1 HW

--- terraform/stage/outputs.tf
+++ terraform/stage/outputs.tf
@@ -5,6 +5,6 @@ output "app_external_ip" {
 #output "app_external_ip_lb" {
 #  value = "${google_compute_forwarding_rule.reddit-app-lb.ip_address}"
 #}
-#output "db_external_ip" {
-#  value = "${module.db.db-address}"
-#}
+output "db_external_ip" {
+  value = "${module.db.db-address}"
+}
